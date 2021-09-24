b185248beef50b412f4ae172bfd3110c063094eb
zhanxw <zhanxw@gmail.com>
2021-09-24 12:58:04 -0500

    Fixed bugs for downloading images.

--- app1.R
+++ app1.R
@@ -7,49 +7,65 @@ library(aplot)
 devtools::install_github("YuLab-SMU/ggtree")
 source('draw-functions.R')
 
-aList=list()
+aList = list()
 
 ui <- fluidPage(
     # Application title
     titlePanel("Tree Data Visualization"),
     
     fluidRow(
-        column(4,
-               fileInput("treefile", "Tree", buttonLabel = "Upload", 
-                         accept = ".nwk")),  # multiple=T: upload multiple files
-        column(4,
-               fileInput("hmfile", "Heatmap", buttonLabel = "Upload", 
-                         accept = ".csv")),  
-        column(4,
-               fileInput("barfile", "Bar Plot", buttonLabel = "Upload", 
-                         accept = ".csv"))  ),
-    fluidRow(
-        column(12, plotOutput("figure"))
+        column(
+            4,
+            fileInput("treefile", "Tree", buttonLabel = "Upload",
+                      accept = ".nwk")
+        ),
+        # multiple=T: upload multiple files
+        column(
+            4,
+            fileInput(
+                "hmfile",
+                "Heatmap",
+                buttonLabel = "Upload",
+                accept = ".csv"
+            )
+        ),
+        column(
+            4,
+            fileInput(
+                "barfile",
+                "Bar Plot",
+                buttonLabel = "Upload",
+                accept = ".csv"
+            )
+        )
     ),
+    fluidRow(column(12, plotOutput("figure"))),
     # TODO download
     downloadButton("download", 'Download')
 )
 
-server <- function(input, output, session) { 
+server <- function(input, output, session) {
     plotInput <- reactive({
         draw(g())
     })
     
-    g<-reactive({   # list
-        cat("g is called,",length(aList),'\n')
+    g <- reactive({
+        # list
+        cat("g is called,", length(aList), '\n')
         print(aList)
-        if (!is.null(input$treefile)){
-            aList[[length(aList)+1]]=list(type='tree', data=read.tree(input$treefile$datapath))
+        if (!is.null(input$treefile)) {
+            aList[[length(aList) + 1]] = list(type = 'tree',
+                                              data = read.tree(input$treefile$datapath))
         }
-        
-        if (!is.null(input$hmfile)){
-            aList[[length(aList)+1]]=list(type='heatmap', data=read.csv(input$hmfile$datapath))
+        if (!is.null(input$hmfile)) {
+            aList[[length(aList) + 1]] = list(type = 'heatmap',
+                                              data = read.csv(input$hmfile$datapath))
         }
-        
-        if (!is.null(input$barfile)){
-            aList[[length(aList)+1]]=list(type='barplot', data=read.csv(input$barfile$datapath))
+        if (!is.null(input$barfile)) {
+            aList[[length(aList) + 1]] = list(type = 'barplot',
+                                              data = read.csv(input$barfile$datapath))
         }
-        #print(aList)
+        print(aList)
     })
     
     output$figure <- renderPlot({
--- draw-functions.R
+++ draw-functions.R
@@ -7,37 +7,41 @@ library(aplot)
 source('helper.R')
 
 drawtree <- function (tr) {
-  ggtree(tr, layout="roundrect", color="firebrick", size=2, ladderize=TRUE) +
-    geom_tiplab(colour="firebrick", size=10, align=TRUE) + 
-    geom_tippoint(color="orange", size=2) +
-    geom_rootedge(color="firebrick", size=2)
+  ggtree(
+    tr,
+    layout = "roundrect",
+    color = "firebrick",
+    size = 2,
+    ladderize = TRUE
+  ) +
+    geom_tiplab(colour = "firebrick",
+                size = 10,
+                align = TRUE) +
+    geom_tippoint(color = "orange", size = 2) +
+    geom_rootedge(color = "firebrick", size = 2)
 }
 
-drawhm <- function (hm){
+drawhm <- function (hm) {
   print(hm)
-  ggplot(hm, aes(x=Category, y=Label)) + 
-    geom_tile(aes(fill=Value)) + scale_fill_viridis_c()
+  ggplot(hm, aes(x = Category, y = Label)) +
+    geom_tile(aes(fill = Value)) + scale_fill_viridis_c()
 }
 
-drawbar <- function (bar){
-  ggplot(bar, aes(Label, Value)) + 
-    geom_col(aes(fill=Label)) + 
-    geom_text(aes(label=Label, y= Value)) + 
-    coord_flip() 
-  
-    ggplot(bar, aes(input$selectedcolx, input$selectedcoly)) + 
-      geom_col(aes(fill=input$selectedcolx)) + 
-      geom_text(aes(label=input$selectedcolx, y=input$selectedcoly)) + 
-      coord_flip()
+drawbar <- function (bar) {
+  ggplot(bar, aes(Label, Value)) +
+    geom_col(aes(fill = Label)) +
+    geom_text(aes(label = Label, y = Value)) +
+    coord_flip()
 }
-  ## TODO: need to deal with names with flexibility
+
+## TODO: need to deal with names with flexibility
 
 draw1 <- function(data) {
-  if (data$type=='tree') {
+  if (data$type == 'tree') {
     drawtree(data$data)
-  } else if (data$type=='heatmap') {
+  } else if (data$type == 'heatmap') {
     drawhm(data$data)
-  } else if (data$type=='barplot') {
+  } else if (data$type == 'barplot') {
     drawbar(data$data)
   }
 }
@@ -49,22 +53,23 @@ draw <- function (data) {
   th = theme(legend.position = "top")
   print(data)
   if (length(data) == 0) {
-    g <- ggplot(data.frame(x = 0, y = 0, 
-                           text = "No data yet")) + geom_text(aes(x = x, y= y, label = text))
-  } else if (length(data)==1){
+    g <- ggplot(data.frame(x = 0, y = 0,
+                           text = "No data yet")) + geom_text(aes(x = x, y = y, label = text))
+  } else if (length(data) == 1) {
     g <- draw1(data[[1]])
-  } else if (length(data)>1) {
+  } else if (length(data) > 1) {
     ## TODO: only keep one figure for tree plot
     # replace by the new tree
     ## TODO: make sure there is at least one tree plot
-      g <- draw1(data[[1]])
-      row.order = get_taxa_order(g) # from top to bottom
-      for (i in 2:length(data)) {
-        data[[i]]$data$Label = factor(data[[i]]$data$Label, level = rev(row.order))
-        g2 <- draw1(data[[i]])
-        g2 <- g2 + theme(axis.title.y = element_blank(), axis.text.y = element_blank())
-        g <- g + g2 * th
-      }
+    g <- draw1(data[[1]])
+    row.order = get_taxa_order(g) # from top to bottom
+    for (i in 2:length(data)) {
+      data[[i]]$data$Label = factor(data[[i]]$data$Label, level = rev(row.order))
+      g2 <- draw1(data[[i]])
+      g2 <-
+        g2 + theme(axis.title.y = element_blank(), axis.text.y = element_blank())
+      g <- g + g2 * th
+    }
   }
   g
 }
@@ -75,7 +80,8 @@ if (T) {
     d = list(
       list(type = 'tree', data = read.tree("tree.nwk")),
       list(type = 'heatmap', data = read.csv("heatmap.csv")),
-      list(type = 'barplot', data = read.csv('bar.csv')))
+      list(type = 'barplot', data = read.csv('bar.csv'))
+    )
     draw(d[1:3])
   }
   testDraw()
