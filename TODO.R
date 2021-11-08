8a481d0bd706209aea2f4bf12a41511715609dbd
Jennifer Liu <jenniferliuyige@gmail.com>
2021-11-08 09:21:46 -0800

    todo updated

--- TODO.R
+++ TODO.R
@@ -1,7 +1,7 @@
 TODO
 
 # 1. two lines plot 
-# 2.Add an editable tab, and allow figure rearrangements. (see NOTE1) more than 3 plots
+# 2. Add an editable tab, and allow figure rearrangements. (see NOTE1) more than 3 plots
 # 3. only one heatmap and one barplot? UI More input?
 
 # 4. select headers
