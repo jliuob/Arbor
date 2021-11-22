3f52d894d1d9710fef65190c20163cd39901bfc0
jliuob <89574127+jliuob@users.noreply.github.com>
2021-11-22 03:01:03 -0800

    Better appearance

--- app1.R
+++ app1.R
@@ -14,19 +14,19 @@ source('draw-functions.R')
 
 debug <- FALSE
 if (debug) {
-  library(reactlog)
-  reactlog_enable()
-  # once app has closed, display reactlog from shiny
-  shiny::reactlogShow()
+    library(reactlog)
+    reactlog_enable()
+    # once app has closed, display reactlog from shiny
+    shiny::reactlogShow()
 } 
 if (FALSE) {
-  library(reactlog)
-  reactlog_disable()
+    library(reactlog)
+    reactlog_disable()
 }
 
 ui <- dashboardPage(
     dashboardHeader(title = "Tree Data Visualization"),
-
+    
     dashboardSidebar(
         sidebarMenu(
             menuItem('Introduction', tabName = 'introduction', icon = icon('book-open')),
@@ -41,10 +41,10 @@ ui <- dashboardPage(
     dashboardBody(
         useShinyjs(),
         tags$head(
-          tags$style(HTML("
+            tags$style(HTML("
                       .shiny-html-output { overflow-x: scroll; }
                       " )
-          )
+            )
         ),
         tabItems(
             tabItem(tabName = "introduction",
@@ -55,157 +55,163 @@ ui <- dashboardPage(
                     for tree data upload. Only csv is accepted for both 
                     heat map and bar plot data upload. Users are also 
                     welcomed to download the plots if needed.'
-                    ),
+            ),
             
             tabItem(tabName = "upload",
                     fluidRow(
-                        column(width = 6,
-                        box(title = 'Step 1: Tree', width = NULL,
-                            status = "primary", solidHeader = TRUE,
-                            fileInput("treefile",
-                                      "Tree", 
-                                      buttonLabel = "Upload",
-                                      accept = ".nwk")
-                        )),
-                    
-                        column(width = 6,
-                        box(title = 'Step 2: Heatmap', width = NULL,
-                            status = "primary", solidHeader = TRUE,
-                            collapsible = TRUE,
-                            fileInput("hmfile",
-                                      "Heatmap",
-                                      buttonLabel = "Upload",
-                                      accept = ".csv",
-                                      multiple = TRUE)
-                        ),
+                        column(width = 4,
+                               box(title = 'Step 1: Tree', width = NULL,
+                                   status = "primary", solidHeader = TRUE,
+                                   fileInput("treefile",
+                                             "Tree", 
+                                             buttonLabel = "Upload",
+                                             accept = ".nwk",
+                                             multiple = FALSE)
+                               )),
                         
-                        box(title = 'Step 2: Bar Plot', width = NULL,
-                            status = "primary", solidHeader = TRUE,
-                            collapsible = TRUE,
-                            fileInput("barfile",
-                                      "Bar Plot",
-                                      buttonLabel = "Upload",
-                                      accept = ".csv",
-                                      multiple = TRUE)
-                        )
-                    )),
-                    fluidRow(
+                        column(width = 4,
+                               box(title = 'Step 2: Heatmap', width = NULL,
+                                   status = "primary", solidHeader = TRUE,
+                                   collapsible = TRUE,
+                                   fileInput("hmfile",
+                                             "Heatmap",
+                                             buttonLabel = "Upload",
+                                             accept = ".csv",
+                                             multiple = TRUE)
+                               )),
+                               
+                        column(width = 4,
+                               box(title = 'Step 2: Bar Plot', width = NULL,
+                                   status = "primary", solidHeader = TRUE,
+                                   collapsible = TRUE,
+                                   fileInput("barfile",
+                                             "Bar Plot",
+                                             buttonLabel = "Upload",
+                                             accept = ".csv",
+                                             multiple = TRUE)
+                               )
+                        )),
+                   
                         box(title = "Upload Status", width = NULL, 
                             status = "primary", solidHeader = TRUE,
                             collapsible = TRUE,
                             verbatimTextOutput("uploadStatusText")
                         )
-                    )
+                    
             ),
             
             tabItem(tabName = 'rearrangements',
                     fluidRow(column(12,
-                    # datatable(
-                    #         barfile, extensions = 'RowReorder',
-                    #         options = list(rowReorder = TRUE, order = list(c(0 , 'asc')))
-                    # )
+                                    # datatable(
+                                    #         barfile, extensions = 'RowReorder',
+                                    #         options = list(rowReorder = TRUE, order = list(c(0 , 'asc')))
+                                    # )
                     ))),
-
+            
             tabItem(tabName = "plot",
                     fluidRow(column(12, uiOutput("figure"))),
                     hr(),
-                fluidRow(
-                  column(12,
-                  box(id = "download.buttons",
-                      title = "Download",
-                    downloadButton("downloadpng", 'Download-png'),
-                    br(),
-                    downloadButton("downloadjpg", 'Download-jpg'),
-                    br(),
-                    downloadButton("downloadpdf", 'Download-pdf')))),
-              
-                fluidRow(column(12,
-                                box(id = "plot.advanced.option",
-                                    title = "Advanced options",
-                                    collapsible = TRUE,
-                                    collapsed = TRUE,
-                             numericInput("plot.width", "Plot width on screen", 500, min = 500)))),
-                
+                    fluidRow(
+                        column(12,
+                               box(id = "download.buttons",
+                                   title = "Download",
+                                   status = "primary", 
+                                   solidHeader = TRUE,
+                                   downloadButton("downloadpng", 'Download-png'),
+                                   br(),
+                                   downloadButton("downloadjpg", 'Download-jpg'),
+                                   br(),
+                                   downloadButton("downloadpdf", 'Download-pdf')))),
+                    
+                    fluidRow(column(12,
+                                    box(id = "plot.advanced.option",
+                                        title = "Advanced options",
+                                        status = "primary",
+                                        solidHeader = TRUE,
+                                        collapsible = TRUE,
+                                        collapsed = TRUE,
+                                        numericInput("plot.width", "Plot width on screen", 500, min = 500)))),
+
             ),
             
             tabItem(tabName = 'contact',
                     "Authors: Xiaowei Zhan, Jennifer Liu.\nGithub: https://github.com/jliuob/Tree.git"
-                    )
-    )
-))
+            )
+        )
+    ))
 
 server <- function(input, output, session) {
-  ## this stores all data
-  v <- reactiveValues(l = list())
-  
-  # ## UI customization
-  #   shinyjs::disable('hmfile')
-  #   shinyjs::disable('barfile')
-  #   observeEvent(input$treefile, {
-  #       enable('hmfile')
-  #       enable('barfile')
-  #   })
-
-
-# Data upload panel --------------------------------------------------------------
+    ## this stores all data
+    v <- reactiveValues(l = list())
+    
+    # ## UI customization
+    #   shinyjs::disable('hmfile')
+    #   shinyjs::disable('barfile')
+    #   observeEvent(input$treefile, {
+    #       enable('hmfile')
+    #       enable('barfile')
+    #   })
+    
+    
+    # Data upload panel --------------------------------------------------------------
     output$uploadStatusText <- renderText({
-      l <- v$l
-      nTree <- length(Filter(function(x){x$type == "tree"}, l))
-      nHeatmap <- length(Filter(function(x){x$type == "heatmap"}, l))
-      nBar <- length(Filter(function(x){x$type == "barplot"}, l))
-      
-      sprintf("Uploaded %d tree plot(s), %d heatmap(s), and %d bar plot(s)",
-              nTree, nHeatmap, nBar)
+        l <- v$l
+        nTree <- length(Filter(function(x){x$type == "tree"}, l))
+        nHeatmap <- length(Filter(function(x){x$type == "heatmap"}, l))
+        nBar <- length(Filter(function(x){x$type == "barplot"}, l))
+        
+        sprintf("Uploaded %d tree plot(s), %d heatmap(s), and %d bar plot(s)",
+                nTree, nHeatmap, nBar)
     })
     observeEvent(input$treefile, {
-      print("obs event: tree")
-      if (!is.null(input$treefile)) {
-        v$l[[length(v$l) + 1]] <- list(type = 'tree',
-                                            data = read.tree(input$treefile$datapath))
-        reset("treefile")
-        cat("input updated by adding a tree", length(v$l), "done\n")
-      }
+        print("obs event: tree")
+        if (!is.null(input$treefile)) {
+            v$l[[length(v$l) + 1]] <- list(type = 'tree',
+                                           data = read.tree(input$treefile$datapath))
+            reset("treefile")
+            cat("input updated by adding a tree", length(v$l), "done\n")
+        }
     })
     observeEvent(input$hmfile, {
-      print("obs event: heatmap")
-      if (!is.null(input$hmfile)) {
-        v$l[[length(v$l) + 1]] <-  list(type = 'heatmap',
-                                        data = read.csv(input$hmfile$datapath))
-        reset("heatmap")
-      }
+        print("obs event: heatmap")
+        if (!is.null(input$hmfile)) {
+            v$l[[length(v$l) + 1]] <-  list(type = 'heatmap',
+                                            data = read.csv(input$hmfile$datapath))
+            reset("heatmap")
+        }
     })
     observeEvent(input$barfile, {
-      print("obs event: barplot")
-      if (!is.null(input$barfile)) {
-        v$l[[length(v$l) + 1]] <- list(type = 'barplot',
-                                       data = read.csv(input$barfile$datapath))
-        reset("barplot")
-      }
+        print("obs event: barplot")
+        if (!is.null(input$barfile)) {
+            v$l[[length(v$l) + 1]] <- list(type = 'barplot',
+                                           data = read.csv(input$barfile$datapath))
+            reset("barplot")
+        }
     })    
-
-# Show figure -------------------------------------------------------------
+    
+    # Show figure -------------------------------------------------------------
     plotWidth <- reactive({
-      if (is.null(input$plot.width)) {
-        500 * max(1, length(v$l))
-      } else {
-        input$plot.width
-      }
+        if (is.null(input$plot.width)) {
+            500 * max(1, length(v$l))
+        } else {
+            input$plot.width
+        }
     })
     plotInput <- reactive({
-      draw(v$l)
+        draw(v$l)
     })  
     output$figure <- renderUI({
-      plotOutput("ggplot", width = plotWidth())
+        plotOutput("ggplot", width = plotWidth())
     })
     
     output$ggplot <- renderPlot({
         draw(v$l)
     }, res = 96)
-     
+    
     get.raster.size <- reactive({
-      size = 8
-      w.h.ratio = max(1, length(v$l))
-      list(height = size / w.h.ratio, width = size)
+        size = 8
+        w.h.ratio = max(1, length(v$l))
+        list(height = size / w.h.ratio, width = size)
     })
     output$downloadpng <- downloadHandler(
         filename = function() {
@@ -229,9 +235,9 @@ server <- function(input, output, session) {
         }
     )
     get.pdf.size <- reactive({
-      size = 8
-      w.h.ratio = max(1, length(v$l))
-      list(height = size / w.h.ratio, width = size)
+        size = 8
+        w.h.ratio = max(1, length(v$l))
+        list(height = size / w.h.ratio, width = size)
     })
     output$downloadpdf <- downloadHandler(
         filename = function() {
@@ -246,4 +252,3 @@ server <- function(input, output, session) {
 }
 
 shinyApp(ui, server)
-
