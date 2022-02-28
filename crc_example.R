ef6860579af476b77effa0be21689c5e59fc8f0a
Jennifer Liu <jenniferliuyige@gmail.com>
2022-02-28 01:31:16 -0800

    crc example

---
+++ crc_example.R
@@ -0,0 +1,11 @@
+source('prepare_data.R')
+source('draw-functions.R')
+
+# ps<-load("ps.Rdata")
+bacteria <- read.csv("~/Documents/Tree/crc/eocrc.vs.ctrl.male.csv")
+crc<-paste("crc")
+
+prepare_data(ps, bacteria, crc)
+
+# label
+# upload github
